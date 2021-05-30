44630c35e3b133c9be78ce2a09f6da76b9398dc5
usmanhaiderfalconit <usmanhaider.falconit@gmail.com>
2021-05-30 16:36:00 +0500

    About Page is added

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1>About this App</h1>
+      <p>App to search Githbu users</p>
+      <p>Version: 1.0.0</p>
+    </>
+  );
+};
+
+export default About;
