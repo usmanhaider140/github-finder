0ccdc0cc2f3986789b452cbd2acc7cd8fa995d4c
usmanhaiderfalconit <usmanhaider.falconit@gmail.com>
2021-05-30 16:35:33 +0500

    User Link is added

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import { Link } from "react-router-dom";
 const UserItem = ({ user: { login, html_url, avatar_url } }) => {
   return (
     <div className="card text-center">
       <img className="circle" src={avatar_url} alt={avatar_url} />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className="btn btn-sm btn-dark my-1">
+        <Link to={`/user/${login}`} className="btn btn-sm btn-dark my-1">
           <i className="fa fa-github"></i> &nbsp; Github Profile
-        </a>
+        </Link>
       </div>
     </div>
   );
