65ce2f66cfc421fd909c42f5c1300f1aec6a6505
usmanhaiderfalconit <usmanhaider.falconit@gmail.com>
2021-06-05 20:13:46 +0500

    Create Alert Context

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import AlertContext from "../../context/alert/alertContext";
+
 const notify = (msg) => toast(msg);
-const Alert = ({ alert }) => {
+
+const Alert = () => {
+  const alertContext = useContext(AlertContext);
+  const { alert } = alertContext;
   return (
     <>
       {alert !== null && notify(alert.msg)} <ToastContainer />
